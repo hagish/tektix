1342a31940e42b3c68842c4812dc70d7303e10b2
Sebastian Dorda <hagish@schattenkind.net>
2014-08-23 13:14:47 +0200

    dummy layout

---
+++ taktik/Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    public int Id;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ taktik/Assets/Scripts/Unit.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+    public enum UnitType {
+        SCISSOR,
+        ROCK,
+        PAPER,
+    }
+
+    public UnitType Type;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
