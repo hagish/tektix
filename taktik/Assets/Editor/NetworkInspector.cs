e9f7575e6b39cd21e8dc4325c75860b7d1a8304a
Sebastian Dorda <hagish@schattenkind.net>
2014-08-23 20:18:54 +0200

    first network gameplay

---
+++ taktik/Assets/Editor/NetworkInspector.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Network))]
+public class NetworkInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (Application.isPlaying)
+        {
+            Network t = (Network)target;
+
+        }
+    }
+}
