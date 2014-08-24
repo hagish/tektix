f4d6fea172a399ff1e549b4b5c1caf318a0141b5
Sebastian Dorda <hagish@schattenkind.net>
2014-08-24 15:16:47 +0200

    rotation on attack

---
+++ taktik/Assets/DetectOther.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectOther : MonoBehaviour {
+    private Unit myUnit;
+
+    void Awake()
+    {
+        myUnit = gameObject.FindComponentUpwards<Unit>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        var otherUnit = other.gameObject.FindComponentUpwards<Unit>();
+        if (otherUnit != null && otherUnit.PlayerId != myUnit.PlayerId && !GameCore.IgnoreEachOther(myUnit, otherUnit))
+        {
+            SendMessageUpwards("OtherDetectedOn");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        var otherUnit = other.gameObject.FindComponentUpwards<Unit>();
+        if (otherUnit != null && otherUnit.PlayerId != myUnit.PlayerId && !GameCore.IgnoreEachOther(myUnit, otherUnit))
+        {
+            SendMessageUpwards("OtherDetectedOff");
+        } 
+    }
+
+}
